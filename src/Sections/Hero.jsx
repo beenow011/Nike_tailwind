9c5821958ee75e32587b4a962bf23bddb9bc6e35
beenow011 <abhinavnb11@gmail.com>
2023-11-19 07:26:02 +0530

    started hero component

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,5 +1,10 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2"
+    ></section>
+  );
 };
 
 export default Hero;
